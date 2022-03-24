a1315397cea09771f2c701a6d4d32fe4af262159
Mohith Reddy <dev.m0hithreddy@gmail.com>
2022-03-25 00:39:51 +0530

    Change the RemoteModule import style similar to importlib.import_module

--- rpyc_mem/client/remote_module.py
+++ rpyc_mem/client/remote_module.py
@@ -7,26 +7,24 @@ class RemoteModule:
 
     :param rpyc_mem.connect.RpycMemConnect rmem_conn: Rpyc memory connection
 
-    .. automethod:: __getattr__
+    .. automethod:: __call__
     """
 
     def __init__(self, rmem_conn):
         """Initialize RemoteModule with rpyc memory connection"""
         self._rmem_conn = rmem_conn
 
-    def __getattr__(self, name):
+    def __call__(self, module=None, package=None):
         """
-        Return ``builtins``/``modules`` of rpyc memory service hosts. Search in remote
-        ``builtins`` before attempting to import ``name`` module.
+        Return the ``modules`` of rpyc memory service hosts. If module is ``None`` assume ``builtins``.
 
-        :param str name: Name of the remote builtins/module
+        :param str module: The module to import in absolute or relative terms (Ex: pkg.mod, ..mod).
+         Defaults to ``builtins``.
+        :param str package: The package which acts as a base for resolving the module (should be set
+         when relative imports are used)
         :return:
         """
-        # Search in remote builtins
-        try:
-            return getattr(self._rmem_conn.remote_import('builtins'), name)
-        except AttributeError:
-            pass
+        if not module:
+            module = 'builtins'
 
-        # Import 'name' module from rmem host
-        return self._rmem_conn.remote_import(name)
+        return self._rmem_conn.remote_import(module, package)
